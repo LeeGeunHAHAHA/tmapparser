7b43b66852f4e64e895e26dc6c0ad7bbd2604b8a
LeeGeunHAHAHA <ygh1kr@naver.com>
2017-11-26 20:48:57 +0900

    change latitude and r

--- 2_PARSE_TMAP/2_tmap_parser.py
+++ 2_PARSE_TMAP/2_tmap_parser.py
@@ -10,14 +10,22 @@ for area_index in XML_list:
     each = IN_DIR + area_index
     print(each)
     fp= codecs.open(each, 'r', encoding='utf-8', errors= 'ignore')
-    out_file = open(OUT_DIR+area_index[:-4]+".txt", 'w')
+    out_file = open(OUT_DIR+"test"+area_index[:-4]+".txt", 'w')
     print(OUT_DIR+area_index[:-4]+".txt")
     mapData = bs(fp,"html.parser")
     for pm in mapData.findAll("placemark"):
         idx = pm.findAll("tmap:congestion")[0].text
         val= pm.findAll("coordinates")[0].text
-        out_file.write(idx+';'+val+'\n')
-        print(idx+';'+val)
+        chg = val.split(" ")
+        for i in range(0,len(chg)):
+            lrp = chg[i].split(",")
+            lrp[0] , lrp[1] = lrp[1], lrp[0]
+            chg[i] = lrp[0] +","+ lrp[1]
+        nval =""
+        for i in chg :
+            nval +=" "+i
+        
+        out_file.write(idx+';'+nval.strip()+'\n')
     out_file.close()
     #print(f)
 
--- 3_SHOW_ON_GOOGLE_MAP/3_show_on_gmap.py
+++ 3_SHOW_ON_GOOGLE_MAP/3_show_on_gmap.py
@@ -1,19 +1,32 @@
 import webbrowser
+import os
 
-GOOGLE_APP_KEY = 'AIzaSyB-IM1lK9obC5m46g11iUd7s5SAGUmUFS0'
+GOOGLE_APP_KEY = 'AIzaSyAEDoAkrq_KdFyPSho9YOnYLstgubov210'
 CENTER = '37.60,126.90'
 
 IN_DIR = '../2_PARSE_TMAP/RESULT/'
 OUT_DIR = './RESULT/'
+con_color ={"0":"white","1":"blue","2":"green","3":"yellow", "4":"orange", "5":"red"}
 
-for area_index in range(16):
-    in_file = open(IN_DIR + "congestion_coor_%02d.txt" % (area_index), 'r')
-    out_file = open(OUT_DIR + "gmap_urlstr_%02d.txt" % (area_index), 'w')
+
+for area_index in os.listdir(IN_DIR):
+    in_file = open(IN_DIR + area_index, 'r')
+    out_file = open(OUT_DIR + area_index, 'w')
+    #print(area_index)
 
     paths = ''
+    #path example : path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397
+
+#https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAEDoAkrq_KdFyPSho9YOnYLstgubov210&center=37.60,126.90&zoom=15&size=600x600&maptype=roadmap&path=color:green|126.913952,37.592662|126.91383,37.592144|126.913698,37.59161|126.913396,37.590471|126.913098,37.589212|126.912991,37.588712
+
+#https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAEDoAkrq_KdFyPSho9YOnYLstgubov210
+    
 
     for line in in_file:
         congestion, linestring = line.split(';')
+        #print(line)
+        paths +="&path=color:"+con_color[congestion]+"|"+linestring.replace(" ","|")
+        #print(paths)
 
         #TOOD: Fill in this block to make color string.
         # congestion 1 --> blue
@@ -35,13 +48,14 @@ for area_index in range(16):
 
 
 
+    #+test url https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAEDoAkrq_KdFyPSho9YOnYLstgubov210&center=37.60,126.90&zoom=12&size=600x600&maptype=roadmap&path=yellow126.916806,37.588333|126.916156,37.588433|126.915932,37.588489|126.915827,37.588525|126.915668,37.588611|126.915468,37.58872|126.915157,37.588951|126.915133,37.588974
 
 
 
+    urlstr = "https://maps.googleapis.com/maps/api/staticmap?key="+ GOOGLE_APP_KEY + "&center=" + CENTER + "&zoom=12&size=600x600&maptype=roadmap" + paths
+    print(urlstr)
 
-    urlstr = ("https://maps.googleapis.com/maps/api/staticmap?key="+ GOOGLE_APP_KEY + "&center=" + CENTER + "&zoom=12&size=600x600&maptype=roadmap" + paths)
-
-    webbrowser.open(urlstr)
+    #webbrowser.open(urlstr)
 
     out_file.write(urlstr)
     out_file.close()
